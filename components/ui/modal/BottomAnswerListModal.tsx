042d478d67afa8982b0e52245f530a9e05fe3a9f
LuisKlopp <fbgus333@gmail.com>
2026-03-16 18:43:03 +0900

    Style: overflow y

--- components/ui/modal/BottomAnswerListModal.tsx
+++ components/ui/modal/BottomAnswerListModal.tsx
@@ -21,7 +21,7 @@ export const BottomAnswerListModal = () => {
 
   return (
     <BottomSheet isOpen={isModal} onClose={closeModal}>
-      <div className="scroll-box flex items-center justify-between px-6 py-4">
+      <div className="flex items-center justify-between px-6 py-4">
         <div>
           <h2 className="text-gray-8 text-lg font-semibold">전체 답변</h2>
           <p className="text-gray-4 text-xs">
@@ -37,7 +37,7 @@ export const BottomAnswerListModal = () => {
         </button>
       </div>
       <div className="bg-primaryNavy/30 mx-auto my-2 h-px w-[40%]" />
-      <div className="space-y-4 px-6 pb-6">
+      <div className="space-y-4 overflow-y-auto px-6 pb-6">
         {data?.map((answer) => (
           <ResponseCard
             key={answer.id}
